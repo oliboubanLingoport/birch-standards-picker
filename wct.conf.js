db83e781f0757f642d2e67d2857a486a2450abfe
Chad Eddington <cdavideddington@gmail.com>
2017-04-11 10:57:27 -0600

    Travis (#34)
    * Set up travis
    
    * Remove bower dep breaking travis builds
    
    * Put the shared mocks back in
    
    * Use the ci user token
    
    * Remove git from url

---
+++ wct.conf.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "plugins": {
+    "sauce": {
+      "disabled": true, 
+      "browsers": [
+        "Windows 8.1/internet explorer",
+        "Windows 10/microsoftedge",
+        "OS X 10.10/safari"
+    ]
+    }
+  }
+}
